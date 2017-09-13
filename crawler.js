5d198680f29983a4f3167de829166836a7aa3356
CJButton <gsyme1907@gmail.com>
2017-09-14 08:48:07 +0900

    crawler can now sift out sets of links and spread out to grab comics from those

--- crawler.js
+++ crawler.js
@@ -8,9 +8,10 @@ const download = require('image-downloader')
 
 const baseUrl = 'https://www.goodreads.com';
 
-const url = "https://www.goodreads.com/book/show/870.Fullmetal_Alchemist_Vol_1";
-
 let visitedUrls = {};
+
+const url = 'https://www.goodreads.com/book/show/870.Fullmetal_Alchemist_Vol_1';
+grabComic(url);
 
 function grabComic(url) {
   request(url, function(error, response, body) {
@@ -20,9 +21,11 @@ function grabComic(url) {
 
     var $ = cheerio.load(body);
 
-    {/* grabs link to comic series list  */}
-    let link = $('a.greyText').attr('href');
+    if (url in visitedUrls) {
+      return;
+    }
 
+    visitedUrls[url] = true;
 
     {/* title  */}
     let title = $('h1.bookTitle').first().contents().filter(function() {
@@ -71,45 +74,43 @@ function grabComic(url) {
         throw err
     });
 
-    // grab link to each comic in the series (and a few more)
-    // can also grab all starting links in 'most popular series'
-    //   fs.appendFileSync('fma.txt', title + '\n' + baseUrl.concat(link) + '\n');
-    //   let title = $(this).find('a.bookTitle > span').text().trim();
+    {/* grabs link to comic series list  */}
+    let link = $('a.greyText').attr('href');
+
+    // console.log(!(baseUrl.concat(link)));
+    if (!(baseUrl.concat(link) in visitedUrls)) {
+      console.log(link);
+      grabLinks(baseUrl.concat(link));
+    }
 
   });
 }
 
-{/* grab all links in a set  */}
-function grabLinks() {
+// const comicList = 'https://www.goodreads.com/list/show/7512.Best_Manga_of_All_Time';
+// const comicList = 'https://www.goodreads.com/series/49276-fullmetal-alchemist'
 
-  // const comicList = 'https://www.goodreads.com/list/show/7512.Best_Manga_of_All_Time';
-  const comicList = 'https://www.goodreads.com/series/49276-fullmetal-alchemist'
+{/* grab all links in a set  */}
+function grabLinks(comicList) {
   request(comicList, function(error, response, body) {
     if(error) {
       console.log("Error: " + error);
     }
 
-    const $ = cheerio.load(body);
+    visitedUrls[comicList] = true;
 
-    let comicLinks = [];
+    const $ = cheerio.load(body);
 
     $('a.bookTitle').each(function( index ) {
       let link = $(this).attr('href');
-      comicLinks.push(baseUrl.concat(link));
+      if (!(baseUrl.concat(link) in visitedUrls)){
+        console.log(link);
+        grabComic(baseUrl.concat(link));
+      }
+      // comicLinks.push(baseUrl.concat(link));
       // fs.appendFileSync('fma.txt', baseUrl.concat(link) + '\n');
     });
-
-    comicLinks.map((comicUrl) => {
-      grabComic(comicUrl)
-    });
-    // grabComic(comicLinks[0]);
-    // from here, we will connect it with a function that grab the comic info
-    // and then create a web grabbing all other comics in the series
-
     console.log("Status code: " + response.statusCode);
   });
-
 }
 
-grabLinks();
-// grabComic(url);
+// grabLinks(comicList);
